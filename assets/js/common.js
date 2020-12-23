dda5ee3448c9b075110b17401e9dcd43e9bc6600
Balakov <mike@mikestimpson.com>
2020-12-23 21:23:03 +0000

    Added the rest of the functionality back from the PHP version.

--- assets/js/common.js
+++ assets/js/common.js
@@ -21,3 +21,16 @@ function ajaxCall(funcName, args, callback) {
 function handleError(error) {
     $('#error-target').html("<div class='alert alert-danger'>" + error + "</div>");
 }
+
+function getUrlVar(name)
+{
+    var pairs = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < pairs.length; i++)
+    {
+        var valueParts = pairs[i].split('=');
+
+        if (valueParts[0] == name) {
+            return valueParts[1];
+        }
+    }
+}
